61b1a20d18fe4ad17c0b9e3acd077eafe50d350f
IslamJenishbekov <islam.akmatov2004@gmail.com>
2025-03-14 01:25:52 +0600

    added multiply

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def add(a, b):
     return a + b
 
+
+def multiply(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     print("Hello from CI/CD demo!")
     print(f"2 + 3 = {add(2, 3)}")
+    print(f"2 * 3 = {multiply(2, 3)}")
